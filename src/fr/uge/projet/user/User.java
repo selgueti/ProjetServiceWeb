8b87ee7510ace0be51bce1ea1f88f1f0974b0bb3
CChevr <cedricus@msn.com>
2022-11-29 15:51:43 +0100

    Modification de l'authentification User
    - Regroupement des Employee et Student sous la classe User
    - Ajout des methodes existsUser & connectionUser
    - Modification hashMap UserManager
    - Modification des fonctions retrieveUser & registerUser

--- src/fr/uge/projet/user/User.java
+++ src/fr/uge/projet/user/User.java
@@ -1,6 +1,36 @@
 package fr.uge.projet.user;
 
-public enum User {
-  STUDENT,
-  EMPLOYEE
+import fr.uge.projet.bike.IBike;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Objects;
+
+public class User extends UnicastRemoteObject implements IUser {
+  private final String pseudo;
+
+  public User(String pseudo) throws RemoteException {
+    super();
+    Objects.requireNonNull(this.pseudo = pseudo);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    if (!super.equals(o)) return false;
+    User user = (User) o;
+    return Objects.equals(pseudo, user.pseudo);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode(), pseudo);
+  }
+
+  @Override
+  public void notifyAvailability(IBike bike) throws RemoteException {
+    Objects.requireNonNull(bike);
+    System.out.println("New location by " + this);
+  }
 }
--- src/fr/uge/projet/user/UserManager.java
+++ src/fr/uge/projet/user/UserManager.java
@@ -4,52 +4,88 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.HashMap;
 import java.util.Objects;
-import java.util.UUID;
 
 /*
  * Thread safe class to manager users
  * */
 public class UserManager extends UnicastRemoteObject implements IUserManager {
+  private final HashMap<String, Pair> usersByPseudo = new HashMap<>();
 
-  private final HashMap<UUID, IUser> usersById = new HashMap<>();
+  private static class Pair {
+      private final IUser user;
+      private int hashedPassword;
+
+    public Pair(IUser user, int hashedPassword) {
+      Objects.requireNonNull(this.user = user);
+      this.hashedPassword = hashedPassword;
+    }
+
+    public IUser getUser() {
+      return user;
+    }
+
+    public int getHashedPassword() {
+      return hashedPassword;
+    }
+  }
 
   protected UserManager() throws RemoteException {
     super();
   }
 
-  @Override
-  public IUser registerUser(User user, String firstName, String lastName) throws RemoteException {
-    UUID id = UUID.randomUUID();
-    Objects.requireNonNull(firstName);
-    Objects.requireNonNull(lastName);
-    synchronized (usersById) {
-      while (usersById.containsKey(id)) {
-        id = UUID.randomUUID();
-      }
-      switch (user) {
-        case STUDENT -> {
-          var student = new Student(id, firstName, lastName);
-          usersById.put(id, student);
-          return student;
-        }
-        case EMPLOYEE -> {
-          var employee = new Employee(id, firstName, lastName);
-          usersById.put(id, employee);
-          return employee;
-        }
-      }
-    }
-    return null;
+  private int hashPassword(String password) {
+    return password.hashCode();
   }
 
   @Override
-  public IUser retrieveUser(UUID id) throws RemoteException {
-    Objects.requireNonNull(id);
-    synchronized (usersById) {
-      if (usersById.containsKey(id)) {
-        return usersById.get(id);
+  public IUser registerUser(String pseudo, String password) throws RemoteException {
+    Objects.requireNonNull(pseudo);
+    Objects.requireNonNull(password);
+
+    synchronized (usersByPseudo) {
+      if (usersByPseudo.containsKey(pseudo))
+        return null;
+
+      IUser user = new User(pseudo);
+      var pair = new Pair(user, hashPassword(password));
+      usersByPseudo.put(pseudo, pair);
+      return user;
+    }
+  }
+
+  @Override
+  public IUser connectionUser(String pseudo, String password) {
+    Objects.requireNonNull(pseudo);
+    Objects.requireNonNull(password);
+
+    synchronized (usersByPseudo) {
+      if (!usersByPseudo.containsKey(pseudo))
+        return null;
+
+      Pair pair = usersByPseudo.get(pseudo);
+      if (pair.getHashedPassword() == hashPassword(password))
+        return pair.getUser();
+      return null;
+    }
+  }
+
+  @Override
+  public boolean existsUser(String pseudo) {
+    Objects.requireNonNull(pseudo);
+
+    return usersByPseudo.containsKey(pseudo);
+  }
+
+  @Override
+  public IUser retrieveUser(String pseudo) throws RemoteException {
+    Objects.requireNonNull(pseudo);
+
+    synchronized (usersByPseudo) {
+      if (usersByPseudo.containsKey(pseudo)) {
+        return usersByPseudo.get(pseudo).user;
       }
     }
+
     return null;
   }
 }
