cb388424f6eb70d7018b3f52e7d92f6cab8db261
Sofian El Guetibi <sofian.el-guetibi@edu.univ-eiffel.fr>
2022-12-04 23:59:05 +0100

    add spring app to setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,9 @@ cd rmi
 
 gnome-terminal -- java -jar bike/build/libs/BikeServer-0.1.0.jar
 gnome-terminal -- java -jar user/build/libs/UserServer-0.1.0.jar
+
+cd ../spring
+mvn package
+gnome-terminal -- java -jar target/spring-mvc-1.0-SNAPSHOT.jar
+sleep 5
+firefox http://localhost:8081/login
